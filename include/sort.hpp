f8f70d055aee96b1016e650201dec67e57ea32e1
Nikita Mikhailovskiy <nmihaylovskiy@gmail.com>
2023-10-30 00:12:29 +0600

    remove all for review

--- include/sort.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "io.hpp"
-
-namespace NHomework2
-{
-    void SortLexicographically(std::vector<std::array<uint8_t, OCTET_NUM>> &pool);
-    void SortLexicographicallyReversed(std::vector<std::array<uint8_t, OCTET_NUM>> &pool);
-} // namespace NHomework2
